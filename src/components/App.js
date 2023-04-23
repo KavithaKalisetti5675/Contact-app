59590f56135b28f52110cf759f2cba3bcb9e2412
kavitha.kalisetti <kavitharao5675@gmail.com>
2023-04-23 14:53:13 +0530

    Render header, addContact and contactList components in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Header } from './Header';
+import { AddContact } from './AddContact';
+import { ContactList } from './ContactList';
+import "./App.css";
 function App() {
+  const contacts = [
+    {
+      id: 1,
+      name: "Kavitha",
+      email: "kavitha@gmail.com"
+    },
+    {
+      id: 2,
+      name: "Ramu",
+      email: "ramu@gmail.com"
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="ui container">
+        {/* <Header/> */}
+        <AddContact/>
+        <ContactList contacts={contacts}/>
+     </div>
   );
 }
 
